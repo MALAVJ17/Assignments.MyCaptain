f7a0c5d2e10440829ac7307ef889bba05c7f363b
MALAVJ17 <122210253+MALAVJ17@users.noreply.github.com>
2023-01-17 21:00:46 +0530

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,18 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+n=0
+print("List 1:",end=" ")
+while n<len(list1):
+    if list1[n]>0:
+        print(list1[n],end=" ")
+    n+=1
+
+print()
+
+    
+m=0
+print("List 2:",end=" ")
+while m<len(list2):
+    if list2[m]>0:
+        print(list2[m],end=" ")
+    m+=1
